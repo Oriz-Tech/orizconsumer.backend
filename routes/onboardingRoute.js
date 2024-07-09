f6d17e9d40aefc1bbbc9754160d6afea050e8c37
Simi <similoluwa.adegoke@kuda.com>
2024-07-09 15:28:58 +0100

    adding google gemini api

--- routes/onboardingRoute.js
+++ routes/onboardingRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const onboardingController = require('../controllers/onboardingController');
 const checkAuthMiddleware = require('../middleware/checkAuthMiddleware');
 const authController = require('../controllers/authController')
+const recommendationController = require("../controllers/recommendationController");
 
 router.post('/api/onboarding/profile', onboardingController.profile);
 router.post('/api/onboarding/verify/email', onboardingController.verifyEmail);
@@ -11,5 +12,6 @@ router.post('/api/onboarding/set/username', checkAuthMiddleware, onboardingContr
 
 router.post('/api/auth/login', authController.userlogin)
 
+router.post('/api/recommendation/generateplan', recommendationController.generatePlan)
 
 module.exports = router;
